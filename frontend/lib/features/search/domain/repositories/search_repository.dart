0ec813114daa8773e0e53c1ae2f4944e72383e0b
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-06-11 05:30:14 -0400

    add future

--- frontend/lib/features/search/domain/repositories/search_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:skill_boost/core/errors/failure.dart';
-import '../entities/search_entity.dart';
-
-abstract class SearchRepository {
-  Future<Either<Failure, List<SearchEntity>>> searchCourses(String query);
-}
