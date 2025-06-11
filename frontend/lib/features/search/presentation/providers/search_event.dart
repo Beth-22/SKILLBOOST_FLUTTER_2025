4926970d136be3df629e43fd7014bb792af18ea4
Netsanet Tibebu <netsanettibebu098@gmail.com>
2025-06-11 05:35:14 -0400

    edit event

--- frontend/lib/features/search/presentation/providers/search_event.dart
+++
@@ -1,8 +0,0 @@
-abstract class SearchEvent {}
-
-class SearchCoursesEvent extends SearchEvent {
-  final String query;
-  SearchCoursesEvent(this.query);
-}
-
-class ClearSearchEvent extends SearchEvent {}
